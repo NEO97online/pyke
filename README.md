b6c4a8faa2b5351c09163e21d19240a3460a8b7f
Michael Auderer <mike@silverstone.io>
2020-04-15 05:13:33 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pyke
+
+Simple Node server to handle GitHub webhooks and pull code after changes are pushed.
---
+++ server.js
@@ -0,0 +1,42 @@
+const crypto = require('crypto')
+const { spawn } = require('child_process')
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const dotenv = fs.readFileSync(path.join('__dirname', '.env'), { encoding: 'utf8' })
+process.env = { ...process.env, dotenv }
+
+const port = process.env.PORT || 6000
+const secret = process.env.PYKE_SECRET
+
+const server = http.createServer((req, res) => {
+  res.writeHead(400, { 'Content-Type': 'application/json' })
+
+  if (req.method !== 'POST') {
+    return res.end(JSON.stringify({ error: 'Invalid request' }))
+  }
+
+  let jsonString = ''
+  req.on('data', data => jsonString += data)
+
+  req.on('end', () => {
+    const hash = 'sha1=' + crypto.createHmac('sha1', secret).update(jsonString).digest('hex') 
+    if (hash !== req.headers['x-hub-signature']) {
+      return res.end(JSON.stringify({ error: 'Invalid key' }))
+    }
+
+    const pyke = spawn(`${__dirname}/pyke`)
+    pyke.stdout.on('data', data => {
+      const buff = new Buffer(data)
+      console.log(buff.toString('utf-8'))
+    })
+
+    res.writeHead(400, { 'Content-Type': 'application/json' })
+
+    return res.end(JSON.stringify({ success: true }))
+  })
+})
+
+server.listen(port, () => console.log(`Pyke hook listening on port ${port}`))
+
